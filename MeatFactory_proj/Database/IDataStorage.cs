e4a9eadc52460936d18087321c7b640ccf5e7ce3
Sima <sima14@i.ua>
2019-06-10 11:54:38 +0300

    trying to do work this shit

--- MeatFactory_proj/Database/IDataStorage.cs
+++ MeatFactory_proj/Database/IDataStorage.cs
@@ -6,17 +6,25 @@ namespace MeatFactory_proj.Database
 {
     interface IDataStorage
     {
+        // select all
         List<Product> selectAllProducts();
         List<Buyer> selectAllBuyers();
         List<Component> selectAllComponents();
         List<Provisioner> selectAllProvisioners();
         List<Transport> selectAllTransports();
 
+        // user
         bool userExists(String login);
         String getPassword(string login);
         User getUser(string login);
         void insertNewUser(string login, string password);
 
+        // component
         List<Component> selectComponentByProductId(string barcode);
+
+        // product
+        void insertNewProduct(Product product);
+        void updateProduct(Product product);
+        void deleteProduct(string barcode);
     }
 }
